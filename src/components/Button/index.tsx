cb66443fa28e7d6398fd37c7f742f3fc5a19cb33
chiziivictor <chizivwokoma@gmail.com>
2022-10-11 16:51:13 +0100

    add flexCenter mixin to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,9 @@
 import './button.scss';
 
-type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
+type ButtonProps = React.ComponentPropsWithoutRef<'button'> & {
+  children: React.ReactNode;
+  className: string;
+};
 
 const Button = ({ children, ...props }: ButtonProps) => {
   return <button {...props}>{children}</button>;
